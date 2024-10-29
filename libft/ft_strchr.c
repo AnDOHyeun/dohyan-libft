012adcdcfeb7fa91496851fcc4491c10f8d496fb
Dohyeon An <dohyan@c2r5s2.42gyeongsan.kr>
2024-10-30 07:46:11 +0900

    10/30

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -23,25 +23,7 @@ char	*ft_strchr(const char *s, int c)
 			return ((char *)(s + i));
 		i++;
 	}
-	if (c == 0)
+	if ((unsigned char)c == 0)
 		return ((char *)(s + i));
 	return (0);
 }
-
-/*
-char    *ft_strchr(const char *s, int c)
-{
-        int             i;
-
-        i = 0;
-        if (c <= 0)
-                return ((char *)(s + i));
-        while (s[i])
-        {
-                if (s[i] == c)
-                        return ((char *)(s + i));
-                i++;
-        }
-        return (0);
-}
-*/
--- libft/libft.h
+++ libft/libft.h
@@ -12,9 +12,14 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
 
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
@@ -26,6 +31,7 @@ int		ft_toupper(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *str);
+//int		ft_lstsize(t_list *lst);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
@@ -36,6 +42,11 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+//void	ft_lstadd_front(t_list **lst, t_list *new);
+//void	ft_lstadd_back(t_list **lst, t_list *new);
+//void	ft_lstiter(t_list *lst, t_list *new);
+//void	ft_lstdelone(t_list *lst, void (*f)(void *));
+//void	ft_lstclear(t_list **lst, void (*del)(void *));
 size_t	ft_strlcat(char *dest, char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
@@ -49,4 +60,7 @@ char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_itoa(int n);
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 char	**ft_split(char *s, char c);
+//t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *)):
+//t_list	*ft_lstnew(void *content);
+//t_list	*lstlast(t_list *lst);
 #endif
